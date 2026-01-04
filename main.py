b51bc2c1faa3be402ad5fbcea80ec47bbe82aa94
Finoana Randriambololona <finoanarandriambololona@MacBook-Air-de-Finoana.local>
2026-01-04 19:13:05 +0300

    post

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,HTTPException
+from sqlalchemy.orm import Session
+import models,schemas
+from database import engine,Base,SessionLocal
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+  
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.post("/books/",response_model=schemas.Book)
+def create_book(book:schemas.BookCreate,db:Session=Depends(get_db)):
+    new_book = models.Book(**book.dit())
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+    return new_book
