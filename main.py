f87b453c2f4748e9e521c27621e55f0f1db4f502
Finoana-Randri <fitiavanarex3@gmail.com>
2026-01-05 22:04:27 +0300

    feat: delete and update book

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def get_db():
 def read_root():
     return {"message": "Hello-World"}
 
+#create a new books
 @app.post("/books/",response_model=schemas.Book)
 def create_book(book:schemas.BookCreate,db:Session=Depends(get_db)):
     new_book = models.Book(**book.dict())
@@ -25,3 +26,41 @@ def create_book(book:schemas.BookCreate,db:Session=Depends(get_db)):
     db.commit()
     db.refresh(new_book)
     return new_book
+#all books
+@app.get("/books/",response_model=list[schemas.Book])
+def get_all_books(skip:int=0, limit:int = 100, db:Session=Depends(get_db)):
+    books= db.query(models.Book).offset(skip).limit(limit).all()
+    return books
+
+# get book ny id
+@app.get("/books/{book_id}",response_model=schemas.Book)
+def get_book(book_id:int, db:Session=Depends(get_db)):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=404,detail="Book Not Found")
+    return book
+
+#update book
+@app.put("/books/{book_id}", response_model=schemas.Book)
+def update_book(book_id:int, updated:schemas.BookCreate, db:Session=Depends(get_db)):
+    book= db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=404,detail="Book Not Found")
+    book.title = updated.title
+    book.author = updated.title
+    book.description = updated.title
+    db.commit()
+    db.refresh(book)
+    return book
+
+#delete book
+@app.delete("/books/{book_id}", response_model=schemas.Book)
+def delete_book(book_id:int , db:Session=Depends(get_db)): 
+    book  = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not book :
+        raise HTTPException(status_code=404, detail="Book Not Found")
+    db.delete(book)
+    db.commit()
+    return book
+
+
